d5def78536200d0f9ea4de6d5d4bcf5e0e686090
Temitopeolumoyegun <temmyt674@gmail.com>
2022-10-13 18:06:09 -0700

    commit 0-7

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return : By defult returns zero if no error (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
