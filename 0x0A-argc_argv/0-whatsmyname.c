23f6279e8f4e87086f5b088f785a1c8315add1b4
Temitopeolumoyegun <temmyt674@gmail.com>
2022-11-06 21:43:48 -0800

    task 0 - 5

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vecto
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	exit(EXIT_SUCCESS);
+}
